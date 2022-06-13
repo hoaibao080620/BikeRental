e529298274db14c013357cd603607636d97906d5
hoaibao080620 <hoaibao08062000@gmail.com>
2022-06-13 23:34:47 +0700

    hhh

--- BikeService.Mercury/Models/BikeReport.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BikeService.Mercury.Models;
-
-// public class BikeReport : BaseEntity
-// {
-//     public string ProblemReport { get; set; }
-//     public TYPE Type { get; set; }
-// }
