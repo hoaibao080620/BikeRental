a4bfd7be52fc27d131a9f07581cb5552764ac2bc
hoaibao080620 <hoaibao08062000@gmail.com>
2022-06-13 22:50:03 +0700

    Update Program.cs

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -4,8 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 builder.Services.AddOktaAuthenticationService(builder.Configuration);
 builder.Services.AddMessageQueueServices();
 builder.Services.AddDbContextService(builder.Configuration);
@@ -17,11 +17,11 @@ await builder.Services.SyncOktaUsers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
