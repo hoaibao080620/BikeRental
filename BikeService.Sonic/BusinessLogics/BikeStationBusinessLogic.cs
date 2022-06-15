46353e424f4a61b3e82a027bba9182d292436142
trehieuhieunguyen <84565314+trehieuhieunguyen@users.noreply.github.com>
2022-06-15 19:46:32 +0700

    hieunguyen
    BikeStation

--- BikeService.Sonic/BusinessLogics/BikeStationBusinessLogic.cs
+++ BikeService.Sonic/BusinessLogics/BikeStationBusinessLogic.cs
@@ -1,6 +1,67 @@
-﻿namespace BikeService.Sonic.BusinessLogics;
+﻿
+using System.Collections;
+using AutoMapper;
+using BikeService.Sonic.DAL;
+using BikeService.Sonic.Dtos;
+using BikeService.Sonic.Dtos.BikeOperation;
+using BikeService.Sonic.Models;
+using BikeService.Sonic.Services.Interfaces;
+
+namespace BikeService.Sonic.BusinessLogics;
 
 public class BikeStationBusinessLogic : IBikeStationBusinessLogic
 {
+    private readonly IBikeLocationHub _bikeLocationHub;
+    private readonly IBikeStationManagerRepository _bikeStationManagerRepository;
+    private readonly IBikeStationRepository _bikeStationRepository;
+    private readonly IMapper _mapper;
+    public BikeStationBusinessLogic(IBikeLocationHub bikeLocationHub, 
+        IBikeStationManagerRepository bikeStationManagerRepository,
+        IBikeRepository bikeRepository,
+        IMapper mapper, 
+        IBikeStationRepository bikeStationRepository)
+    {
+        _bikeLocationHub = bikeLocationHub;
+        _bikeStationManagerRepository = bikeStationManagerRepository;
+        
+        _mapper = mapper;
+        _bikeStationRepository = bikeStationRepository;
+    }
+
+
+    public async Task<BikeStationRetrieveDto> GetStationBike(int id)
+    {
     
+        var stationBike =await _bikeStationRepository.GetById(id);
+        return _mapper.Map<BikeStationRetrieveDto>(stationBike);
+    }
+
+    public async Task<List<BikeStationRetrieveDto>> GetAllStationBikes()
+    {
+        var stationBikes = await _bikeStationRepository.All();
+        return _mapper.Map<List<BikeStationRetrieveDto>>(stationBikes);
+    }
+
+    public async Task AddStationBike(BikeStationInsertDto bikeInsertDto)
+    {
+        await _bikeStationRepository.Add(_mapper.Map<BikeStation>(bikeInsertDto));
+        await _bikeStationRepository.SaveChanges();
+    }
+
+    public async Task DeleteStationBike(int id)
+    {
+        var bikeStation = await _bikeStationRepository.GetById(id);
+        if (bikeStation is null) throw new InvalidOperationException();
+        
+        await _bikeStationRepository.Delete(bikeStation);
+        await _bikeStationRepository.SaveChanges();
+    }
+
+    public async Task UpdateStationBike(BikeStationUpdateDto bikeInsertDto)
+    {
+        await _bikeStationRepository.Update(_mapper.Map<BikeStation>(bikeInsertDto));
+        await _bikeStationRepository.SaveChanges();
+    }
+
+  
 }
--- BikeService.Sonic/Controllers/BikeStationController.cs
+++ BikeService.Sonic/Controllers/BikeStationController.cs
@@ -1,144 +1,56 @@
-﻿// using System.Collections.Concurrent;
-// using System.Text;
-// using BikeService.Sonic.Dtos;
-// using BikeService.Sonic.Extensions;
-// using BikeService.Sonic.Models;
-// using BikeService.Sonic.Services.Interfaces;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.Extensions.Caching.Distributed;
-// using MongoDB.Driver;
-// using Nest;
-// using Newtonsoft.Json;
-// using SharpCompress.Compressors;
-// using SharpCompress.Compressors.Deflate;
-//
-// namespace BikeService.Sonic.Controllers;
-//
-// [ApiController]
-// [Route("[controller]/[action]")]
-// public class BikeStationController : ControllerBase
-// {
-//     private readonly IElasticClient _elasticClient;
-//     private readonly IElasticSearchService _elasticSearchService;
-//     private readonly IDistributedCache _distributedCache;
-//     private readonly IMongoDatabase _mongoDatabase;
-//
-//     public BikeStationController(
-//         IElasticClient elasticClient,
-//         IElasticSearchService elasticSearchService,
-//         IDistributedCache distributedCache,
-//         IMongoDatabase mongoDatabase)
-//     {
-//         _elasticClient = elasticClient;
-//         _elasticSearchService = elasticSearchService;
-//         _distributedCache = distributedCache;
-//         _mongoDatabase = mongoDatabase;
-//     }
-//
-//     [HttpPost]
-//     public async Task<IActionResult> CreateDummyData()
-//     {
-//         var list = new List<BikeStation>();
-//         const string cacheKey = "customerList";
-//         for (var i = 0; i < 500; i++)
-//         {
-//             var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new BikeStation
-//             {
-//                 Id = i
-//             }));
-//             
-//             await _distributedCache.SetAsync($"{cacheKey} {i}", bytes);
-//             
-//             list.Add(new BikeStation
-//             {
-//                 Id = i + 1,
-//                 Name = Guid.NewGuid().ToString()
-//             });
-//         }
-//         
-//         // var collection = _mongoDatabase.GetCollection<BikeStation>("Bike");
-//         // await collection.InsertManyAsync(list);
-//         //
-//         // var stations = new List<BikeStationSearchDto>
-//         // {
-//         //     new BikeStationSearchDto
-//         //     {
-//         //         Id = 1,
-//         //         NameNormalize = "Tram so 4 Pham Van Dong",
-//         //         DescriptionNormalize = "Doi dien DHKT Ho Chi Minh",
-//         //         AddressNormalize = "14 Pham Van Dong, Ho Chi Minh"
-//         //     },
-//         //     new BikeStationSearchDto
-//         //     {
-//         //         Id = 2,
-//         //         NameNormalize = "Tram so 17 Pham Ngu Lao",
-//         //         DescriptionNormalize = "Doi dien quan nuong ba Hang",
-//         //         AddressNormalize = "156 Pham Ngu Lao, Ho Chi Minh"
-//         //     }
-//         // };
-//         //
-//         // foreach (var station in stations)
-//         // {
-//         //     await _elasticClient.IndexDocumentAsync(station);
-//         // }
-//
-//         return Ok();
-//     }
-//
-//     [HttpGet]
-//     public async Task<IActionResult> GetRedisDataTest()
-//     {
-//         const string cacheKey = "customerList";
-//         var customerList = new ConcurrentBag<BikeStation>();
-//         
-//         var tasks = new List<Task>();
-//
-//         for (var i = 0; i < 500; i++)
-//         {
-//             tasks.Add(AddToList($"{cacheKey} {i}", customerList));
-//         }
-//
-//         await Task.WhenAll(tasks);
-//         
-//         return Ok(customerList);
-//     }
-//     
-//     [HttpGet]
-//     public async Task<IActionResult> GetRedisData()
-//     {
-//         const string cacheKey = "customerList";
-//         var customerList = new List<BikeStation>();
-//         
-//         for (var i = 0; i < 500; i++)
-//         {
-//             var cache = await _distributedCache.GetAsync($"{cacheKey} {i}");
-//             if(cache is null) continue;
-//             var value = JsonConvert.DeserializeObject<BikeStation>(Encoding.UTF8.GetString(cache));
-//             customerList.Add(value);
-//         }
-//
-//         return Ok(customerList);
-//     }
-//     
-//     [HttpGet]
-//     public async Task<IActionResult> GetMongoDbData()
-//     {
-//         var collection = _mongoDatabase.GetCollection<BikeStation>("Bike");
-//         var list = await collection.Find(x => true).ToListAsync();
-//         
-//         return Ok(list);
-//     }
-//
-//     private async Task AddToList(string cacheKey, ConcurrentBag<BikeStation> bikeStations)
-//     {
-//         var cache = await _distributedCache.GetAsync(cacheKey);
-//         var value = JsonConvert.DeserializeObject<BikeStation>(Encoding.UTF8.GetString(cache));
-//         bikeStations.Add(value);
-//     }
-//     
-//     [HttpGet]
-//     public async Task<IActionResult> Search([FromQuery] string queryString)
-//     {
-//         return Ok(await _elasticSearchService.SearchBikeStationRecord(queryString));
-//     }
-// }
+﻿using BikeService.Sonic.BusinessLogics;
+using BikeService.Sonic.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace BikeService.Sonic.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+[Authorize]
+public class BikeStationController : ControllerBase
+{
+    private readonly IBikeStationBusinessLogic _bikeStationBusinessLogic;
+    public BikeStationController(IBikeStationBusinessLogic bikeStationBusinessLogic)
+    {
+        _bikeStationBusinessLogic = bikeStationBusinessLogic;
+    }
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public async Task<IActionResult> GetBikeStation(int id)
+    {
+        var bikeStation = await _bikeStationBusinessLogic.GetStationBike(id);
+        return Ok(bikeStation);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllBikeStations()
+    {
+        var bikeStations = await _bikeStationBusinessLogic.GetAllStationBikes();
+        return Ok(bikeStations);
+    }
+    
+    [HttpPost]
+    [Route("[action]")]
+    public async Task<IActionResult> AddStationBike(BikeStationInsertDto stationInsertDto)
+    {
+        await _bikeStationBusinessLogic.AddStationBike(stationInsertDto);
+        return Ok(stationInsertDto);
+    }
+    
+    [HttpPut]
+    public async Task<IActionResult> UpdateStationBike(BikeStationUpdateDto bikeStationUpdateDto)
+    {
+        await _bikeStationBusinessLogic.UpdateStationBike(bikeStationUpdateDto);
+
+        return Ok();
+    }
+    
+    [HttpDelete]
+    public async Task<IActionResult> DeleteStationBike(int id)
+    {
+        await _bikeStationBusinessLogic.DeleteStationBike(id);
+
+        return Ok();
+    }
+}
