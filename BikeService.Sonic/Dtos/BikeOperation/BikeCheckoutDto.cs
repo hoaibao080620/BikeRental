e65b21e1b24d2efb7f59e472c535988add3a5735
hoaibao080620 <hoaibao08062000@gmail.com>
2022-06-29 17:53:27 +0700

    fix bug when create bike station

--- BikeService.Sonic/Dtos/BikeOperation/BikeCheckoutDto.cs
+++ BikeService.Sonic/Dtos/BikeOperation/BikeCheckoutDto.cs
@@ -6,6 +6,6 @@ public class BikeCheckoutDto
     public double Longitude { get; set; }
     public double Latitude { get; set; }
     public int BikeStationId { get; set; }
-    public DateTime CheckoutOn { get; set; }
+    public DateTime CheckoutOn { get; set; } = DateTime.UtcNow;
     public string? Address { get; set; }
 }
