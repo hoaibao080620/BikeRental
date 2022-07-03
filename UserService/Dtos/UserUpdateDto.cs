0333defbb0a792684cef568e6288c9ab275d490f
hoaibao080620 <hoaibao08062000@gmail.com>
2022-07-03 23:12:34 +0700

    Update UserUpdateDto.cs

--- UserService/Dtos/UserUpdateDto.cs
+++ UserService/Dtos/UserUpdateDto.cs
@@ -4,7 +4,7 @@ public class UserUpdateDto
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
-    public string? Address { get; set; }
+    public string? Address { get; set; } = null;
     public string? PhoneNumber { get; set; } = null;
     public DateTime? DateOfBirth { get; set; } = null;
     public string RoleName { get; set; }
