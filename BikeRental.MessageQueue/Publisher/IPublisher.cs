cc34cabbc06c6c58fbfe05f2acbdabc84368cbb1
hoaibao080620 <hoaibao08062000@gmail.com>
2022-08-14 08:56:52 +0700

    t

--- BikeRental.MessageQueue/Publisher/IPublisher.cs
+++
@@ -1,8 +0,0 @@
-﻿using Amazon.SimpleNotificationService.Model;
-
-namespace BikeRental.MessageQueue.Publisher;
-
-public interface IPublisher
-{
-    public Task SendMessage(string message, string topicArn, Dictionary<string, MessageAttributeValue>? messageAttributes = null);
-}
