e92079a01dc0842c4f45827495bc50801bb17d4c
hoaibao080620 <hoaibao08062000@gmail.com>
2022-08-21 21:33:08 +0700

    update

--- UserService/Dtos/UserInsertDto.cs
+++ UserService/Dtos/UserInsertDto.cs
@@ -9,8 +9,7 @@ public class UserInsertDto
     [Required]
     public string LastName { get; set; } = null!;
     public string? Address { get; set; }
-    [Required]
-    public string Email { get; set; } = null!;
+    public string? Email { get; set; } = null!;
     public string? PhoneNumber { get; set; } = null!;
     public DateTime? DateOfBirth { get; set; }
     [Required]
